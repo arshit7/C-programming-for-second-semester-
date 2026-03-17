a6644824a9e9f9337898d61427ac5b6d2df4c947
Arshit Thakur <th.aarshit.1718@gmail.com>
2026-03-17 12:37:22 +0530

    Add files via upload

---
+++ dimondprob.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+/*    A
+     / \
+    B   C
+     \ /
+      D
+     */
+class A {
+public:
+    void show() {
+        cout << "This is class A" << endl;
+    }
+};
+
+class B : virtual public A {};
+class C : virtual public A {};
+
+class D : public B, public C {};
+
+int main() {
+    D obj;
+    obj.show();
+    return 0;
+}
